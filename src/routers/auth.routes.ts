84ca9b15caa50e34964a10c53b262786f3178ddf
Nguyentatho <nguyentatho14106@gmail.com>
2025-12-04 13:04:24 +0700

    'add'

--- src/routers/auth.routes.ts
+++ src/routers/auth.routes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
-import { createAccount, getUser, login } from "../handlers/auth.handle";
+import { changePassword, createAccount, getUser, login } from "../handlers/auth.handle";
 
 const router = Router();
 router.get("/:token", getUser);
 router.post("/", createAccount);
 router.post("/login", login);
+router.patch("/changepass", changePassword);
 
 export default router;
